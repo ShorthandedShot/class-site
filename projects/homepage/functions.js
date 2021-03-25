852aac5e1ac307fb5419d1d391748ba5c010fa46
Jayce Nguyen <32528654+ShorthandedShot@users.noreply.github.com>
2021-03-25 00:01:41 -0400

    hmmm

--- projects/homepage/functions.js
+++ projects/homepage/functions.js
@@ -235,3 +235,21 @@ function createDiv(height, width) {
 $( function() {
   $( ".draggable" ).draggable();
 } );
+
+//collapsible
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+//soundddddd
